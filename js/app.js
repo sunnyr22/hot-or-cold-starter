971293996b750deb148e425f77ed43595fc55335
Sundeep <sunnyr22@gmail.com>
2015-07-27 23:02:17 -0400

    Added feedback and other functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,51 @@
 /*-----Global variables----*/
 	
 	var count = 0;
+	var computerChoice = 0;
+	var won = false;
+
+	function newGame() {
+	computerChoice = Math.floor((Math.random() * 100) + 1);
+    console.log(computerChoice);
+    $('#userGuess').val("");
+    count = 0;
+    $('#count').text(count);
+    $("#guessList").empty(); 
+    won = false;
+    $("#feedback").text("Make your Guesses");
+}
+
+
+	function guessCompare (guess){
+		var diff = Math.abs(computerChoice -guess);
+		console.log (diff);
+		if(diff === 0){
+			$("#feedback").text("Congrats! You Won.");
+			won = true;
+		}
+		else if (diff <= 10){
+			$("#feedback").text("Very Hot");
+		}
+		else if (diff <= 20){
+			$("#feedback").text("Hot");
+		}
+		else if (diff <= 30){
+			$("#feedback").text("Warm");
+		}
+		else if (diff <= 50){
+			$("#feedback").text("Cold");
+		}
+		else if (diff <= 100){
+			$("#feedback").text("Artic Cold");
+		}
+
+	}
+
+
+
 
 $(document).ready(function(){
-
+	newGame();
 
 	/* ---Starting a new game --*/
 	// $('document').on("click", ".new",function(){
@@ -11,7 +53,7 @@ $(document).ready(function(){
 	// });
 
 	$('.new').click(function () {
-      history.go(0);
+      newGame();
     });
 
 
@@ -27,36 +69,47 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	})
 
-// --- Computer generated number-------
-  	var computerChoice = Math.floor((Math.random() * 100) + 1);
-    console.log(computerChoice);
- 
 
 
  //  ----Log and count user Guesses-------
  	$('#guessButton').click(function (e) {
  		e.preventDefault();
+ 		if (won) {
+ 			alert("Please start new game");
+ 			return;
+ 		}
+
  		var guess = parseInt($('#userGuess').val());  /* MAKE THIS GLOBAL ???*/
- 		// var count = 0;  *******ASK THIS *******
 	  		count += 1;
  		if(guess <= 100 && guess >0) {
 	  		$('#guessList').append('<li>' + guess + '</li>');
-	  		$('#userGuess').val();  /* question about this?????????*/
+	  		$('#userGuess').val("");  /* question about this?????????*/
 	  		$('#count').text(count);
-	  	}
-	  	else if(isNaN(guess) || guess >=100){
+	  		guessCompare(guess);
+	  	} 
+
+	  	else {
 	  		alert("Please enter a number between 0 and 100")
 	  	}
-	  	document.getElementById('userGuess').value='';
+
   	})
 
 
-  	// ------ User Hot- Cold Guidance----//
-  	// var diff = Math.abs(computerChoice -guess);
-  	// console.log (diff);
-
+  	
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
